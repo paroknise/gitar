63eeb1169d7fdb0065c59398c4ad26d0f804323b
norbadrul <norbadrul@live.com>
2020-02-05 12:50:27 +0800

    100 LoginLogout

---
+++ client/src/routes.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+
+import Layout from './hoc/layout';
+import Auth from './hoc/auth'
+import RegisterLogin from './components/Register_login';
+import Register from './components/Register_login/register';
+
+import UserDashboard from './components/user';
+
+
+
+const Routes = () => {
+  return(
+    <Layout>
+      <Switch>
+        <Route path="/user/dashboard" exact component={Auth(UserDashboard,true)}/>
+        <Route path="/register" exact component={Auth(Register,false)}/>
+        <Route path="/register_login" exact component={Auth(RegisterLogin,false)}/>
+        <Route path="/" exact component={Auth(Home,null)}/>
+      </Switch>
+    </Layout>
+      
+
+  )   
+}
+
+export default Routes;
