89676cfa826d5baa26a907c745827d922376ce65
norbadrul <norbadrul@live.com>
2020-03-24 14:14:37 +0800

    server changes

--- server/server.js
+++ server/server.js
@@ -10,12 +10,14 @@ const async = require('async');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-//mongoose.connect(process.env.MONGODB_URI)
-mongoose.connect(process.env.DATABASE)
+mongoose.connect(process.env.MONGODB_URI)
+
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use(express.static('client/build'))
 
 cloudinary.config({
     cloud_name:process.env.CLOUD_NAME,
@@ -443,7 +445,15 @@ app.post('/api/users/update_profile', auth,(req,res)=>{
 //             })
 //         }
 //     )
-// })  
+// })
+
+//DEFAULT
+if(process.env.NODE_ENV === 'production'){
+    const path = require('path');
+    app.get('/*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname,'../client','build', 'index.html'))
+    })
+}
 
 
 const port = process.env.PORT || 3002;
