58f300bfb23505891ca5e85a0a0c2e2dba5d10a2
norbadrul <norbadrul@live.com>
2020-04-26 12:14:21 +0800

    server changes

--- server/server.js
+++ server/server.js
@@ -422,30 +422,30 @@ app.post('/api/users/update_profile', auth,(req,res)=>{
 });
 
 //=================================
-//              SITE
+             // SITE
 //=================================
 
-// app.get('/api/site/site_data',(req,res)=>{
-//     Site.find({},(err,site)=>{
-//         if(err) return res.status(400).send(err);
-//         res.status(200).send(site[0].siteInfo)
-//     });
-// });
+app.get('/api/site/site_data',(req,res)=>{
+    Site.find({},(err,site)=>{
+        if(err) return res.status(400).send(err);
+        res.status(200).send(site[0].siteInfo)
+    });
+});
 
-// app.post('/api/site/site_data',auth,admin,(req,res)=>{
-//     Site.findOneAndUpdate(
-//         { name: 'Site'},
-//         { "$set": {siteInfo: req.body}},
-//         { new: true },
-//         (err,doc )=>{
-//             if(err) return res.json({success:false,err});
-//             return res.status(200).send({
-//                 success: true,
-//                 siteInfo: doc.siteInfo
-//             })
-//         }
-//     )
-// })
+app.post('/api/site/site_data',auth,admin,(req,res)=>{
+    Site.findOneAndUpdate(
+        { name: 'Site'},
+        { "$set": {siteInfo: req.body}},
+        { new: true },
+        (err,doc )=>{
+            if(err) return res.json({success:false,err});
+            return res.status(200).send({
+                success: true,
+                siteInfo: doc.siteInfo
+            })
+        }
+    )
+})
 
 //DEFAULT
 if(process.env.NODE_ENV === 'production'){
